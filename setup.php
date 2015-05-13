db8fd275ac27597892536fec81765e836f0ad0f8
Harrie Wheatley <harriegwheatley@gmail.com>
2015-05-13 13:23:57 +0100

    Added uninstall option and improved error handling.

--- setup.php
+++ setup.php
@@ -12,7 +12,7 @@ if($_GET['setup']==1){
 				verified int(1)
 				)";
 		if($db->query($qry)){
-			//$w = fopen("setup_complete.ver", "true");
+			$w = fopen("setup_complete.ver", "true");
 			echo "Setup completed correctly. Table created.";
 		} else {
 			echo "Setup failed. Check the settings.php file. It is likely that you have entered your DB connection details incorrectly. <br><br>" . $qry;
@@ -20,8 +20,31 @@ if($_GET['setup']==1){
 	}else{
 		echo "One or more of your fields was empty in settings.php";
 	}
+} elseif ($_GET['uninstall']==2) {
+	if($_POST['submit']){
+		$del = "DROP TABLE " . $_SESSION['ver_tablename'];
+		if($db->query($del)){
+			$verdir = scandir('');
+			foreach($verdir as $file) {
+				if(is_file($file)){
+					unlink($file);
+	  				echo $file . " deleted. <br>";
+				}
+			}
+		}
+	}
 } else {
-	echo "No setup flag specified.";
+	if(file_exists ("setup_complete.ver")){
+		echo "You have Verfico installed correctly. You can uninstall by using the ?uninstall=1 flag.";
+	} else {
+		echo "Verfico is not installed on this server. <br>";
+		echo "Run with the ?setup=1 flag to begin setup.";
+	}
 }
 
 ?>
+
+<form action="setup.php?uninstall=1" method="post">
+	Warning: Uninstalling Verfico will completely remove all Verfico files and tables from your server and database.
+	<input type="submit" value="Click here to confirm Verfico uninstallation." style="border:none;background-color:red;" name="submit">
+</form>
